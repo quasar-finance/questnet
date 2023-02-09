015ffff700902558b5901911703dd8c12879baeb
Pablo <118397961+pabloqsr@users.noreply.github.com>
2023-02-09 04:43:26 -0300

    Added v04 files (#35)

---
+++ v04/utils/build_persistent_peers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ef
+
+PEERS_FILE="persistent_peers.txt"
+
+
+if [ "$#" -ne 1 ]; then
+    echo "Pass the tendermind node as parameter and it will build the persistent_peers parameter for you"
+    exit 1
+fi
+
+FILTERED_PEERS=$(sed "/^$1/d" "$PEERS_FILE" | paste -sd,)
+echo "persistent_peers = \"$FILTERED_PEERS\""
+
